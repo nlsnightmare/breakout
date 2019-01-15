a25403e72e886c2cc7333c49d7116bcb9fdb76b3
Νίκος Μαρουλάκης <nlsnightmare@gmail.com>
2019-01-15 15:49:48 +0200

    updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,13 @@ module.exports = {
     entry: './src/code.js',
     mode: 'development',
     output:{
-	filename: 'bundle.js',
-	path: path.resolve(__dirname,'dist')
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'dist')
     },
     devServer:{
-	overlay:true,
-	contentBase: path.join(__dirname,'dist'),
-	compress: true,
-	port:8001
+        overlay:true,
+        contentBase: path.join(__dirname,'dist'),
+        compress: true,
+        port:8001
     },
 };
